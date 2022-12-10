9e2d902c9e4ce0174bcea32464b5607b8e5a7652
Szymon <szym.burkowicz@gmail.com>
2022-12-10 21:05:31 +0100

    RadekChanges

--- src/Components/IndexPage/ContentText.js
+++ src/Components/IndexPage/ContentText.js
@@ -6,8 +6,8 @@ export default function ContentText()
                 <h1 className="title-h1">Cześć, jestem Radek!</h1>
                 <h1 className="title-h1">projektant graficzny & freelancer</h1>
             </div>
-            <div >
-                <h3 className="small-text" >#identyfikacja wizualna #logo #web design</h3>
+            <div>
+                <h3 className="small-text" >#identyfikacja wizualna &nbsp;#logo &nbsp;#web design</h3>
             </div>
         </div>
     )
