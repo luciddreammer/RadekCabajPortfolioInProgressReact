6fc391db9dfed9056ca6c2601200c292838abf57
Szymon <szym.burkowicz@gmail.com>
2023-01-14 23:07:43 +0100

    Contact changes

--- src/Components/ContactPage/ContactRight.js
+++ src/Components/ContactPage/ContactRight.js
@@ -6,14 +6,12 @@ export default function ContactRight()
     return(
         <div className="contact-right">
             <div id="forms">
+                <div className='form-text-main'> Formularz kontaktowy</div>
                     <form action="https://formsubmit.co/luciddreammer@gmail.com" method="POST" >
-                        <div >Name</div>
-                        <textarea type="text" name="name"  id="single-form" placeholder="Your name..."></textarea>
-                        <div >E-mail</div>
-                        <textarea type="text" name="email" id="single-form" placeholder="Your e-mail adress..."></textarea>
-                        <div>Message</div>
-                        <textarea type="text" name="message" id="single-form-third" placeholder="Your message..."></textarea>
-                        <button type="submit" id="custom-btn">SEND</button>
+                        <textarea className='contact-form' type="text" name="name"  id="single-form" placeholder="Twoje imię..."></textarea><br></br>
+                        <textarea className='contact-form' type="text" name="email" id="single-form" placeholder="Twój adres e-mail..."></textarea><br></br>
+                        <textarea className='contact-form' type="text" name="message" id="single-form-third" placeholder="Treść Twojej wiadomości..."></textarea>
+                        <br></br><button type="submit" id="custom-btn">Wyślij</button>
                     </form>
                 </div>
         </div>
